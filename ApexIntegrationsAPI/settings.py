a16717dc9c1aa15e4d70f7465c7c5ea1a5fa35cd
Ian Schoenrock <ianschoenrock@gmail.com>
2026-03-30 12:11:31 -0600

    Added static root

--- ApexIntegrationsAPI/settings.py
+++ ApexIntegrationsAPI/settings.py
@@ -133,6 +133,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 2. Where Django should look for your custom files (like your PDF)
+# Since you created a 'static' folder right in your base directory, we add it here.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
